7857d21f189e2f0cbf30ec1f001e5f4a67b2c719
Tosin-Mayowa <toss800@gmail.com>
2025-06-10 14:33:15 +0100

    projects updates

--- src/app/projects/page.tsx
+++ src/app/projects/page.tsx
@@ -25,7 +25,7 @@ export default function Projects() {
             </p>
             <Link
               href={`/projects/${project.id}`}
-              className="transition-all duration-500 ease-in-out w-[60%] 
+              className="transition-all duration-500 text-white ease-in-out w-[60%] 
                 text-center opacity-100 xl:opacity-0 group-hover:opacity-100 
                 h-[20px] md:h-[40px] flex justify-center items-center mt-3 text-[13px] rounded-[4px] md:rounded-[8px] self-center bg-primary "
             >
