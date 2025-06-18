b0bbee81e5bf55f56684524e8913a14a49a1bbbd
Tosin-Mayowa <toss800@gmail.com>
2025-06-18 01:03:09 +0100

    fonts added

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -2,7 +2,14 @@ import type { Metadata } from "next";
 import "./globals.css";
 import { Header } from "@/Components/Header/Header";
 import { ThemeProvider } from "./_lib/Context";
+import { Playfair_Display } from "next/font/google";
 
+const playfair = Playfair_Display({
+  subsets: ["latin"],
+  weight: ["400", "500", "700"], // choose based on your usage
+  variable: "--font-playfair",   // optional, for CSS variable usage
+  display: "swap",
+});
 export const metadata: Metadata = {
   title: "Mayowa Portfolio",
   description: "Frontend Engineer with 3years experience",
@@ -16,7 +23,7 @@ export default function RootLayout({
   return (
     <html lang="en">
       <body
-        
+        className={playfair.className}
       >
         <ThemeProvider>
          <Header/>
